5ae99ee49aa08ba2dd50a2dc6577f72f0cbdb15b
Aredhel13 <55153102+Aredhel13@users.noreply.github.com>
2019-11-14 13:05:48 +0300

    add authorisation
    And added login and password to the DB

--- Client/Client.dpr
+++ Client/Client.dpr
@@ -4,13 +4,15 @@ uses
   Vcl.Forms,
   main in 'main.pas' {fmMain},
   AddOrder in 'AddOrder.pas' {fmOrder},
-  test in 'test.pas' {Form1};
+  test in 'test.pas' {Form1},
+  Authorization in 'Authorization.pas' {Form2};
 
 {$R *.res}
 
 begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm2, Form2);
   Application.CreateForm(TfmMain, fmMain);
   Application.CreateForm(TForm1, Form1);
   Application.CreateForm(TfmOrder, fmOrder);
---
+++ Client/__recovery/Authorization.pas
@@ -0,0 +1,86 @@
+unit Authorization;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Data.DB, Data.SqlExpr, Vcl.StdCtrls,
+  Data.DBXFirebird;
+
+type
+  TForm2 = class(TForm)
+    Label1: TLabel;
+    Label2: TLabel;
+    Label3: TLabel;
+    Edit1: TEdit;
+    Edit2: TEdit;
+    Button1: TButton;
+    Button2: TButton;
+    SQLConnection1: TSQLConnection;
+    Button3: TButton;
+    procedure Button1Click(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
+    procedure Button3MouseDown(Sender: TObject; Button: TMouseButton;
+      Shift: TShiftState; X, Y: Integer);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form2: TForm2;
+  f : boolean = false;
+
+implementation
+
+{$R *.dfm}
+uses main;
+
+procedure TForm2.Button2Click(Sender: TObject);
+begin
+if ((Length(Edit1.Text)=0) or (Length(Edit1.Text)=0)) then
+  ShowMessage('Имя пользователя или пароль не могут быть пустыми!')
+  else
+with SQLConnection1 do
+  begin
+    Params.Values['Database']:='D:\Системная\Desktop\elephantSoftware\Authentication\ELEPHANTSOFTWARE.FDB';
+    Params.Values['Login']:=Edit1.Text;
+    Params.Values['Password']:=Edit2.Text;
+    Params.Values['SQLDialect']:='3';
+  end;
+  try
+    Screen.Cursor := crSQLWait;
+    SQLConnection1.Open;
+    Screen.Cursor := crDefault;
+    ModalResult := mrOk;
+    fmMain.ShowModal;
+  except
+    Screen.Cursor := crDefault;
+    MessageDlg('Неверно указан '+'логин или пароль. Попробуйте ещё раз',mtError,[mbOk],0);
+    Edit1.SetFocus;
+  end;
+end;
+
+procedure TForm2.Button1Click(Sender: TObject);
+begin
+   Edit1.clear;
+   Edit2.clear;
+end;
+
+procedure TForm2.Button3MouseDown(Sender: TObject; Button: TMouseButton;
+  Shift: TShiftState; X, Y: Integer);
+begin
+if (f=false) then
+begin
+Edit2.PasswordChar := #0;
+f:=true;
+end
+else
+begin
+Edit2.PasswordChar := #42;
+f:=false;
+end
+end;
+
+end.
